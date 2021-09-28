75ceb5e95d06516880b8daba14733b072a23f5dd
unknown <ashokkumar.pesarlanka@gmail.com>
2021-09-28 16:29:45 +0530

    Code Coverage screenshots

--- test_app.py
+++ test_app.py
@@ -24,3 +24,37 @@ def test_pred_virginica():
         # asserting the correct response is received
         assert response.status_code == 200
         assert response.json() == {"flower_class": "Iris Virginica"}
+
+# test to check if Iris Setosa is classified correctly
+def test_pred_IrisSetosa():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 4.9,
+        "sepal_width": 3,
+        "petal_length": 1.4,
+        "petal_width": 0.2,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+
+        # asserting the correct response is received
+        assert response.status_code == 200
+        json_data = response.json()
+        assert json_data['flower_class'] == "Iris Setosa"
+        
+# test to check if Iris Versicolour is classified correctly
+def test_pred_IrisVersicolour():
+    # defining a sample payload for the testcase
+    payload = {
+        "sepal_length": 6.4,
+        "sepal_width": 3.2,
+        "petal_length": 4.5,
+        "petal_width": 1.5,
+    }
+    with TestClient(app) as client:
+        response = client.post("/predict_flower", json=payload)
+
+        # asserting the correct response is received
+        assert response.status_code == 200
+        json_data = response.json()
+        assert json_data['flower_class'] == "Iris Versicolour"
